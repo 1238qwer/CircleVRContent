3b20fc6388f1cb0de2d58673ceb6d8d1c22ace20
1238qwer <1238qwer@naver.com>
2018-06-27 13:59:17 +0900

    first

---
+++ Assets/Player.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    [SerializeField] private float moveSpeed;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(0, 0, moveSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(0, 0, -moveSpeed * Time.deltaTime);
+        }
+    }
+}
